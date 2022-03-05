3f0373895975cc43766aa24283973037c3e11841
Teen Yang <chopcostop@gmail.com>
2022-03-05 18:03:09 +0800

    fix: render ProductPage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import ProductPage from './product/ProductPage.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProductPage />
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );

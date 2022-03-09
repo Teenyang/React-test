c627d3040a89552d2d0c083c4a368f990dc67ee8
Teen Yang <chopcostop@gmail.com>
2022-03-09 12:49:04 +0800

    add: GamePage rewrited with function component

---
+++ src/game/GamePage.js
@@ -0,0 +1,94 @@
+import './GamePage.css';
+import { useState } from 'react';
+
+const Square = ({ square, handleClick }) => (
+  <button className="square" onClick={handleClick}>
+    {square}
+  </button>
+);
+
+const Board = ({ squares, handleClick }) => {
+  const renderSquare = (index) => {
+    return (
+      <Square square={squares[index]} handleClick={() => handleClick(index)} />
+    )
+  }
+
+  return (
+    <div>
+      {/* <div className="status">{status}</div> */}
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+const Game = () => {
+  // 基本九宮格陣列
+  const initSquares = Array(9).fill(null);
+
+  // 歷史紀錄：初始第0局即為基本九宮格陣列
+  const [history, setHistory] = useState([[...initSquares]]);
+  console.log('history: ', history);
+
+  // 遊戲盤數累加
+  const [stepNumber, setStepNumber] = useState(0);
+
+  // 初始player1為Ｏ：xIsNext = false
+  const [xIsNext, setNext] = useState(false);
+
+  // 目前局面：倒數最後一組歷史紀錄
+  const currentSquare = history[history.length - 1];
+  console.log('currentSquare: ', currentSquare);
+
+  // 下一步局面：延續目前局面
+  let squares = [...currentSquare];
+
+  const HandleClick = (index) => {
+    console.log('HandleClick index: ', index);
+    // 若點擊格內已有圖案，則取消執行
+    if (squares[index]) return;
+    // 決定圖案
+    squares[index] = xIsNext ? 'Ｘ' : 'Ｏ';
+    // 切換玩家
+    setNext(!xIsNext);
+    // 遞增遊戲盤數
+    setStepNumber(stepNumber + 1);
+    // 更新歷史紀錄：每次點擊後，都在歷史紀錄的最後段併入下一步局面
+    setHistory([...history].concat([squares]));
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={currentSquare} handleClick={HandleClick} />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default function GamePage() {
+  return (
+    <div className="GamePage">
+      <h1>Tic-Tac-Toe</h1>
+      <Game />
+    </div>
+  );
+}

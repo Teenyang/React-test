facf0715e0734614c6d2774e0d6268585e16ab29
Teen Yang <chopcostop@gmail.com>
2022-03-14 20:22:38 +0800

    fix: import useLocation, render Link by pathname

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-// import { Outlet, Link } from "react-router-dom";
-import { Outlet, Link, NavLink } from "react-router-dom";
-import { useState } from 'react';
+import { Outlet, Link, NavLink, useLocation } from "react-router-dom";
 
 function App() {
-  const [isHome, setHome] = useState(true);
+  let location = useLocation();
+  console.log('location: ', location);
 
   return (
     <div className="App">
-      <header className={`App-header ${!isHome ? 'App-header-top' : ''}`}>
-        {/* <header className={`App-header ${navToTop}`}> */}
+      <header className={`App-header ${location.pathname !== '/' ? 'App-header-top' : ''}`}>
         < img src={logo} className="App-logo" alt="logo" />
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -25,13 +23,15 @@ function App() {
         </a> */}
 
         <nav>
-          {!isHome ? (<Link to="/" onClick={() => setHome(true)}>Home</Link>) : ''}
-          <NavLink to="/product" onClick={() => setHome(false)}
+          {(location.pathname !== '/') ? (<Link to="/">Home</Link>) : ''}
+
+          <NavLink to="/product"
             className={({ isActive }) => isActive ? 'active-link' : undefined}
           >
             Product List
           </NavLink>
-          <NavLink to="/game" onClick={() => setHome(false)}
+
+          <NavLink to="/game"
             className={({ isActive }) => isActive ? 'active-link' : undefined}
           >
             Tic-Tac-Toe Game
